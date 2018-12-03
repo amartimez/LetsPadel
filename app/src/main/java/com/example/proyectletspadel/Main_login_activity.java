2cd08ed59f6d772934bd30ce3947e6c9683c5129
amartimez <amartimez@elpuig.xeill.net>
2018-12-04 00:50:10 +0100

    Primer commit

--- app/src/main/java/com/example/proyectletspadel/Main_login_activity.java
+++ app/src/main/java/com/example/proyectletspadel/Main_login_activity.java
@@ -1,13 +1,52 @@
 package com.example.proyectletspadel;
 
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Main_login_activity extends AppCompatActivity {
-
+    ImageButton IvFacebook;
+    ImageButton IvGmail;
+    ImageButton IvRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login_activity);
+
+        IvFacebook=findViewById(R.id.ivFacebook);
+        IvGmail=findViewById(R.id.ivGmail);
+        IvRegistro=findViewById(R.id.ivLogging);
+
+        IvFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_login_activity.this,StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        IvGmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_login_activity.this,StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        IvRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_login_activity.this,Login_Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/example/proyectletspadel/PersonalLoggin.java
+++ app/src/main/java/com/example/proyectletspadel/PersonalLoggin.java
@@ -1,13 +1,76 @@
 package com.example.proyectletspadel;
 
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class PersonalLoggin extends AppCompatActivity {
-
+   Button btnAceptar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+
+
+        );
+        ParseInstallation.getCurrentInstallation().saveInBackground();
         setContentView(R.layout.activity_personal_loggin);
+        final EditText userTxt;
+        final EditText PasswordTxt;
+
+        btnAceptar= findViewById(R.id.buttonAceptar);
+        userTxt = findViewById(R.id.PersonalUsuario);
+        PasswordTxt = findViewById(R.id.PersonalContraseña);
+
+
+        ParseUser user = new ParseUser();
+// Set the user's username and password, which can be obtained by a forms
+        user.setUsername(String.valueOf(userTxt));
+        user.setPassword(String.valueOf(PasswordTxt));
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(PersonalLoggin.this,"Solo te llevará 15 segundos!! :)", Toast.LENGTH_LONG).show();
+                } else {
+                    ParseUser.logOut();
+                    Toast.makeText(PersonalLoggin.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(PersonalLoggin.this,StartActivity.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
