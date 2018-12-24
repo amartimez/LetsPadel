5cbd228096f291dc41391f4b0b4dc57f7770acbd
amartimez <amartimez@elpuig.xeill.net>
2018-12-24 18:02:21 +0100

    Finalizado calendario buscar partida y crear nueva, y crear partida nueva falla el enlace de buscar partida al reclicler view...

--- app/src/main/java/com/example/proyectletspadel/MenuFragment.java
+++ app/src/main/java/com/example/proyectletspadel/MenuFragment.java
@@ -14,29 +14,81 @@ import android.widget.Button;
 
 public class MenuFragment extends Fragment {
 
+    private Button BtnBuscarPartida;
+    private Button BtnoNuevaPartida;
+
+    public MenuFragment(){
+
+
+    }
 
     @Override
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Palas activity = (Palas) getActivity();
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        View rootView=inflater.inflate(R.layout.fragment_menu, container, false);
+
+        BtnBuscarPartida= rootView.findViewById(R.id.busca_Partida);
+        BtnoNuevaPartida= rootView.findViewById(R.id.crea_Partida);
+       // return inflater.inflate(R.layout.fragment_menu, container, false);
 
 
+        BtnBuscarPartida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendData();
+            }
+        });
+
+        BtnoNuevaPartida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendData2();
+            }
+        });
+
+        return rootView;
 
 
         }
 
-   /* public boolean onNavigation (MenuItem item_id){
+    private void sendData()
+    {
+        //INTENT OBJ
+        Intent i = new Intent(getActivity().getBaseContext(),
+                Calendario.class);
 
-       / FragmentManager FragmentManager = getSupportFragmentManager();
+        //PACK DATA
+      /*  i.putExtra("SENDER_KEY", "MyFragment");
+        i.putExtra("NAME_KEY", nameFragTxt.getText().toString());
+        i.putExtra("YEAR_KEY", Integer.valueOf(launchYearSpinner.getSelectedItem().toString()));*/
 
-        int id = item_id.getItemId();
+        //RESET WIDGETS
+       // nameFragTxt.setText("");
+        //launchYearSpinner.setSelection(0);
 
-        if (id == R.id.busca_Partida) {
+        //START ACTIVITY
+        getActivity().startActivity(i);
+    }
 
-            FragmentManager.beginTransaction().replace(R.id.contenedor,new AyudaFragment()).commit();
+    private void sendData2()
+    {
+        //INTENT OBJ
+        Intent i = new Intent(getActivity().getBaseContext(),
+                Palas.class);
 
-        }*/
+        //PACK DATA
+      /*  i.putExtra("SENDER_KEY", "MyFragment");
+        i.putExtra("NAME_KEY", nameFragTxt.getText().toString());
+        i.putExtra("YEAR_KEY", Integer.valueOf(launchYearSpinner.getSelectedItem().toString()));*/
+
+        //RESET WIDGETS
+        // nameFragTxt.setText("");
+        //launchYearSpinner.setSelection(0);
+
+        //START ACTIVITY
+        getActivity().startActivity(i);
+    }
+
     }
